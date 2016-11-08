d7203f24c70fa6ca91eac6c6f60ba8255b081b2e
robin <robin@asus>
2016-11-08 12:47:07 +0100

    commit exo2

--- app/src/main/java/com/example/robin/exo2_tp/MainActivity.java
+++ app/src/main/java/com/example/robin/exo2_tp/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.robin.exo2_tp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
+    String[] noms_onglets= new String[] {"ABC", "DEF", "GHI"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listview= (ListView) findViewById(R.id.list);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, noms_onglets);
+        listview.setAdapter(adapter);
     }
 }
